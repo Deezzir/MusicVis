4c9c49fc83d281f6bcf12c7636e4e5c0ff4350f8
Deezzir <deezzir@gmail.com>
2023-10-13 17:40:36 -0400

    added FFT for better vis

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
+#include <assert.h>
+#include <complex.h>
+#include <math.h>
+#include <raylib.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <stdint.h>
 
-#include <raylib.h>
-
-#define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+#define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
+#define N 256
 
 typedef struct {
     float left;
@@ -17,21 +19,51 @@ const int WIDTH = 800;
 const int HEIGHT = 450;
 const char* MUSIC_PATH = "The Tower of Dreams (new).ogg";
 
-Frame global_frames[4800*2];
+float in[N];
+float complex out[N];
+float max_amp;
+Frame global_frames[4800 * 2];
 size_t global_frames_count = 0;
 
-void callback(void *bufferData, unsigned int frames) {
-    size_t capacity = ARRAY_LEN(global_frames);
+float amp(float complex v) {
+    float a = fabsf(crealf(v));
+    float b = fabsf(cimagf(v));
+    if (a < b) return b;
+    return a;
+}
 
-    if (frames <= capacity - global_frames_count) {
-        memcpy(global_frames + global_frames_count, bufferData, sizeof(Frame)*frames);
-        global_frames_count += frames;
-    } else if (frames <= capacity) {
-        memmove(global_frames, global_frames + frames, sizeof(Frame)*(capacity - frames));
-        memcpy(global_frames + (capacity - frames), bufferData, sizeof(Frame)*frames);
-    } else {
-        memcpy(global_frames, bufferData, sizeof(Frame)*capacity);
-        global_frames_count = capacity;
+void fft(float in[], size_t stride, float complex out[], size_t n) {
+    if (n == 1) {
+        out[0] = in[0];
+        return;
+    }
+
+    fft(in, stride * 2, out, n / 2);
+    fft(in + stride, stride * 2, out + n / 2, n / 2);
+
+    for (size_t k = 0; k < n / 2; ++k) {
+        float t = (float)k / n;
+        float complex v = cexp(-2 * I * PI * t) * out[k + n / 2];
+        float complex e = out[k];
+        out[k] = e + v;
+        out[k + n / 2] = e - v;
+    }
+}
+
+void callback(void* bufferData, unsigned int frames) {
+    if (frames < N) return;
+
+    Frame* frame_data = bufferData;
+    for (size_t i = 0; i < frames; ++i) {
+        in[i] = frame_data[i].left;
+    }
+
+    fft(in, 1, out, N);
+
+    max_amp = 0.0f;
+    for (size_t i = 0; i < frames; ++i) {
+        float a = amp(out[i]);
+        if (max_amp < a) max_amp = a;
     }
 }
 
@@ -53,7 +85,6 @@ int main(void) {
     SetMusicVolume(music, 0.5f);
     AttachAudioStreamProcessor(music.stream, callback);
 
-
     while (!WindowShouldClose()) {
         UpdateMusicStream(music);
 
@@ -70,15 +101,11 @@ int main(void) {
 
         BeginDrawing();
         {
-            ClearBackground(CLITERAL(Color) {0x0, 0x0, 0x55, 0xFF});
-            float cell_width = (float)w / global_frames_count;
-            for (size_t i = 0; i < global_frames_count; ++i) {
-                float t = global_frames[i].left;
-                if (t > 0.0f) {
-                    DrawRectangle(i*cell_width, h/2 - h/2*t, 1, h/2*t, RED);
-                } else {
-                    DrawRectangle(i*cell_width, h/2, 1, h/2*t, RED);
-                } 
+            ClearBackground(CLITERAL(Color){0x0, 0x0, 0x55, 0xFF});
+            float cell_width = (float)w / N;
+            for (size_t i = 0; i < N; ++i) {
+                float t = amp(out[i]);
+                DrawRectangle(i * cell_width, h / 2 - h / 2 * t, cell_width, h / 2 * t, RED);
             }
         }
         EndDrawing();
